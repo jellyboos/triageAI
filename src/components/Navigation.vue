913c7ec04bc3d8b34dd89ffc39551181189fbee9
johny <johnyang3794@gmail.com>
2025-05-17 14:18:19 -0700

    Refactor App.vue to use Navigation component and remove HelloWorld.vue
    - Replaced dynamic component rendering in App.vue with a static Navigation component.
    - Deleted HelloWorld.vue as it was no longer needed.

---
+++ src/components/Navigation.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, computed } from 'vue'
+import LandingPage from './LandingPage.vue'
+import PatientPage from './PatientPage.vue'
+import NursePage from './NursePage.vue'
+
+const currentPage = ref('landing')
+const navigateTo = (page) => {
+  currentPage.value = page
+}
+
+const currentComponent = computed(() => {
+  switch (currentPage.value) {
+    case 'landing':
+      return LandingPage
+    case 'patient':
+      return PatientPage
+    case 'nurse':
+      return NursePage
+    default:
+      return LandingPage
+  }
+})
+</script>
+
+<template>
+  <component :is="currentComponent" @navigate="navigateTo" />
+</template>
